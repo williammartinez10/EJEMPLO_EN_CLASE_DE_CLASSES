0092c59210f460b01937266c065d0879d58b0fc9
williammartinez10 <william.martinez10@upr.edu>
2021-02-04 23:12:43 -0400

    json files corrected

---
+++ corroboration.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+    string myStr = "I am practicing how to save changes";
+    int number = 2021;
+    cout << myStr << ' ' << number << endl;
+    cout << "William is my name" << endl;
+    return 0;
+}
